66e7285c928f9fd70e58febe4e1a2cb959ae40fe
Mitsuharu Emoto <mthr1982@gmail.com>
2022-12-02 18:49:09 +0900

    Initial Commit

---
+++ LintDrivenDevSample/LintDrivenDevSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LintDrivenDevSampleApp.swift
+//  LintDrivenDevSample
+//
+//  Created by Mitsuharu Emoto on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct LintDrivenDevSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
